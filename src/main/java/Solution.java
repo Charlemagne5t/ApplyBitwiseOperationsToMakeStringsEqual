1435f723a6cd4c8e1d96770f4df4e10bb25c3489
alek <wait82985@gmail.com>
2024-07-06 20:07:18 +0500

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,27 @@
 public class Solution {
     public boolean makeStringsEqual(String s, String target) {
 
+        int s1 = 0;
+
+        int t1 = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '0') {
+
+            }else s1++;
+            if(target.charAt(i) == '0') {
+
+            }else t1++;
+        }
+        if(s1 != 0 && t1 == 0){
+            return false;
+        }
+
+        if(s1 == 0 && t1 != 0){
+            return false;
+        }
+
+
+
+        return true;
     }
 }
